47c75aa73f2251c3dd57a262ba486387439cdc86
KyleY <kyle.yu.96@outlook.com>
2020-04-01 22:38:57 -0400

    make code ready to deploy to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://blog-rails-kyle.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://blog-rails-kyle.herokuapp.com', 'http://blog-rails-kyle.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
