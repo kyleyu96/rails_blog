3854254bd4cd29fa859007b89b5092c776a60ac7
KyleY <kyle.yu.96@outlook.com>
2020-04-07 22:05:39 -0400

    adjust article title length

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   has_many :article_categories
   has_many :categories, through: :article_categories
   has_many :comments, dependent: :destroy
-  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   validates :body, presence: true, length: { minimum: 50 }
   validates :user_id, presence: true
   default_scope -> { order(created_at: :desc) }
